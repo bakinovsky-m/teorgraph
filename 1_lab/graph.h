5581bdf0a5c4b2fbc59e02cb4f31808015aea9cf
bakinovsky-m <makbak@ya.ru>
2017-09-28 14:41:56 +0300

    1 laba

---
+++ 1_lab/graph.h
@@ -0,0 +1,133 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <vector>
+#include <iostream>
+#include <stdio.h>
+
+class Graph{
+public:
+	Graph(int count, std::vector<int> vi, std::vector<int> vj);
+
+	void printToConsole() const;
+	void printToConsoleTEXTOM() const;
+	void add(int i, int j);
+	void exportToDot(char *) const;
+private:
+	void computeHL();
+	std::vector<int> IJ, H, L;
+	int count = 0;
+};
+
+Graph::Graph(int _count, std::vector<int> vi, std::vector<int> vj){
+	count = _count;
+	IJ.reserve(vj.size() * 2);
+	H.reserve(count);
+	L.reserve(vi.size() * 2);
+	for(int i = 0; i < vi.size(); i++){
+		IJ.push_back(vi[i]);
+	}
+	for(int i = 0; i < vj.size(); i++){
+		IJ.push_back(vj[vj.size() - 1 - i]);
+	}
+	for(int i = 0; i < count; i++){
+		H.push_back(-1);
+	}
+	for(int i = 0; i < IJ.size(); i++){
+		L.push_back(-1);
+	}
+
+	computeHL();
+}
+
+void Graph::add(int i, int j){
+	IJ.insert(IJ.begin() + IJ.size() / 2, i);
+	IJ.insert(IJ.begin() + (IJ.size() + 1) / 2, j);
+	computeHL();
+}
+
+void Graph::computeHL(){
+	for(int i = 0; i < IJ.size(); i++){
+		int k = IJ[i];
+		L[i] = H[k];
+		H[k] = i;
+	}
+}
+
+void Graph::printToConsole() const{
+	for(int i = 0; i < IJ.size() / 2; i++){
+		std::cout << IJ[i] << " ";
+	}
+
+	std::cout << std::endl;
+
+	for(int i = 0; i < IJ.size() / 2; i++){
+		std::cout << IJ[IJ.size() - 1 - i] << " ";
+	}
+	std::cout << std::endl;
+
+	for(int i = 0; i < H.size(); i++){
+		std::cout << (H[i]) << " ";
+	}
+
+	std::cout << std::endl;
+	for(int i = 0; i < L.size(); i++){
+		std::cout << (L[i]) << " ";
+	}
+	std::cout << std::endl;
+}
+
+void Graph::printToConsoleTEXTOM() const{
+	for (int i = 0; i < H.size(); i++){
+		std::vector<int> v;
+
+		for(int k = H[i]; k != -1;){
+			v.push_back(k);
+			k = L[k];
+		}
+
+		std::cout << "from " << i << ": ";
+		for(int i = 0; i < v.size(); i++){
+			std::cout << v[i] << ", ";
+		}
+		std::cout << std::endl;
+	}
+}
+// void Graph::printToConsole() const{
+// 	std::cout << "ind IJ  H  L" << std::endl;
+// 	for(int i = 0; i < IJ.size(); i++){
+// 		std::cout << i << " |";
+
+// 		std::cout << IJ[i] << " |";
+// 		std::cout << IJ[IJ.size() - 1 - i] << " |";
+
+// 		if(i >= H.size() - 1){
+// 			std::cout << "  |";
+// 		} else {
+// 			std::cout << H[i] << " |";
+// 		}
+// 		if(i >= L.size() - 1){
+// 			std::cout << "  ";
+// 		} else {
+// 			std::cout << L[i] << " ";
+// 		}
+// 		std::cout << std::endl;
+// 	}
+// 	std::cout << std::endl;
+// }
+
+void Graph::exportToDot(char * filename) const{
+	FILE *out;
+	out = fopen(filename, "w");
+	fprintf(out, "graph gr {\n");
+	for(int i = 0; i < count; i++){
+		fprintf(out, "  %d;\n", i);
+	}
+	for(int i = 0; i < IJ.size() / 2; i++){
+		fprintf(out, "  %d -- %d;\n", IJ[i], IJ[IJ.size() - 1 - i]);
+	}
+	fprintf(out, "}\n");
+	fclose(out);
+}
+
+#endif // GRAPH_H
---
+++ 1_lab/main.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+#include "graph.h"
+
+using namespace std;
+
+int main(){
+	int count = 5;
+	vector<int> vi = {0, 1, 0, 3, 3, 4, 2};
+	vector<int> vj = {1, 3, 2, 2, 4, 2, 4};
+
+	Graph g = Graph(count, vi, vj);
+	g.printToConsole();
+	g.printToConsoleTEXTOM();
+	g.exportToDot("my_graph1.dot");
+	g.add(1, 2);
+	g.exportToDot("my_graph2.dot");
+	
+
+	return 0;
+}
